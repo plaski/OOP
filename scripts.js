3d0aa418d19fe0426a4ebf07e000118c04669f45
plaski <maciejploskonka@gmail.com>
2017-03-13 21:32:45 +0100

    first part of todolist

--- scripts.js
+++ scripts.js
@@ -1,16 +1,51 @@
-var Iphone5S = new Telefon("Apple", 2500, "czarny");
-var SamsungGalaxyS6 = new Telefon("Samsung", "350000", "piaskowy beż");
-var OnePlusOne = new Telefon("OnePlus(?)", "zagadka", "biel");
-function Telefon(marka, cena, kolor) {
-	this.marka = marka;
-	this.cena = cena;
-	this.kolor = kolor;
+$(function() {
 
-	Telefon.prototype.printInfo = function() {
-		console.log("Marka telefonu to " + this.marka + ", cena to " + this.cena + ", a kolor to " + this.kolor + ".");
-	};
+})
+
+function randomString() {
+    var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var str = '';
+    var i = 0;
+    for (i = 0; i < 10; i++) {
+        str += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return str;
+}
+
+function Column(name) {
+	var self = this;
+
+	this.id = randomString();
+	this.name = name;
+	this.$element = createColumn();
+
+	function createColumn() {
+		var $column = $('<div').addClass('column');
+		var $columnTitle = $('<h2>').addClass('column-title').text(self.name);
+		var $columnCardList = $('<ul>').addClass('column-card-list');
+		var $columnDelete = $('<button>').addClass('btn-delete').text('X');
+		var $columnAddCard = $('<button>').addClass('add-card').text('Dodaj kartę');
+
+		$columnDelete.click(function() {
+			self.removeColumn();
+		});
+		$columnAddCard.click(function() {
+			self.addCard(new Card(prompt("podaj nazwę karty")))
+		});
+
+		$column.append($columnTitle)
+				.append($columnDelete)
+				.append($columnAddCard)
+				.append($columnCardList);
+		return $column;
+	}
+}
+
+Column.prototype = {
+	addCard: function(card) {
+		this.$element.children('ul').append(card.$element);
+	},
+	removeColumn: function() {
+		this.$element.remove();
+	}
 };
-
-Iphone5S.printInfo();
-SamsungGalaxyS6.printInfo();
-OnePlusOne.printInfo();
